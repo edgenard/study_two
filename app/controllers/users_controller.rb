b6aa97d98165d6f9233a05a1f8dd59d7bf3f058c
Emmanuel Genard <edgenard@gmail.com>
2015-05-12 12:09:48 -0400

    Working on users and sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   
+  
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      login! @user
+      flash[:notice] = "Welcome to StudyTwo!"
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
   
   def new
@@ -11,14 +20,23 @@ class UsersController < ApplicationController
   
   
   def edit
-    
+    @user = current_user
+    render :edit
   end
   
   def update
-    
+    @user = current_user
+    if @user.update(user_params)
+      flash.now[:notice] = "Successfuly Saved"
+      render :edit
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :edit
+    end
   end
   
   def destroy
+    @user = current_user
     
   end
   
