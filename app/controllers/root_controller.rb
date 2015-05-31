daed10343209f2e5189aba2f5fa49743ca28c015
Emmanuel Genard <edgenard@gmail.com>
2015-05-31 18:05:32 -0400

    Fix header link, Edit CardForm

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,12 +1,8 @@
 class RootController < ApplicationController
-  before_action :check_currentuser
+
   def root
     render :welcome
   end
-  
-  private 
-  def check_currentuser
-    redirect_to study_space_url if current_user
-  end
+
   
 end
