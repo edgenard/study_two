c3ae502bd9bee539931488c7637f6fc429d54a62
Emmanuel Genard <edgenard@gmail.com>
2015-11-21 09:48:01 -0500

    Test for invalid signups with bad passwords is passing

--- spec/features/signups_spec.rb
+++ spec/features/signups_spec.rb
@@ -3,13 +3,35 @@ require 'rails_helper'
 RSpec.feature "Signups", type: :feature do
  scenario "sign up with valid email and password" do
    visit  "users/new"
-
    fill_in "user[email]", with: "user@example.com"
    fill_in "user[password]", with: "password"
 
-   click_button 'Get Beiged'
+   click_button "Get Beiged"
 
   expect(page).to have_content "Study Space"
  end
+
+ describe "invalid signups" do
+   scenario " password that is too short" do
+     visit "users/new"
+     fill_in "user[email]", with: "user@example.com"
+     fill_in "user[password]", with: "pass"
+
+     click_button "Get Beiged"
+
+     expect(page).to have_content "Password is too short"
+   end
+
+   scenario "password is blank" do
+     visit "users/new"
+     fill_in "user[email]", with: "user@example.com"
+     fill_in "user[password]", with: ""
+
+     click_button "Get Beiged"
+
+     expect(page).to have_content "Password is too short"
+   end
+ end
+
 end
 
